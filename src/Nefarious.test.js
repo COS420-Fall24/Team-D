25c24929764b39fb61f2c824418eea423c4ee020
Greg Nelson <gregory.nelson@maine.edu>
2024-11-12 23:37:44 -0500

    NEFARIOUS COMMIT FAILING TEST FAIL

---
+++ src/Nefarious.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders sign-in button', () => {
+  render(<App />);
+  const buttonElement = screen.getByText(/sign in with google/i);
+  expect(buttonElement).not.toBeInTheDocument();
+});
+
