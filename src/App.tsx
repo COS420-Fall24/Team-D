bba50f8e2e60b7631290ccb8f19782f2d4d2a893
clsc411 <clarascalzer@gmail.com>
2024-11-17 14:42:04 -0500

    seperated login component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
 import React from 'react';
 import './App.css';
-import { auth } from './firebase-config';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import Login from './pages/Login'; 
 
 
-function App() {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-  const handleSignIn = async () => {
-    try {
-      await signInWithGoogle();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+const App: React.FC = () => {
   return (
     <div className="App">
-      {user ? (
-        <div>
-          <p>Welcome, {user.user.displayName}</p>
-          <button onClick={() => auth.signOut()}>Sign Out</button>
-        </div>
-      ) : (
-        <button onClick={handleSignIn}>Sign In with Google</button>
-      )}
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { auth } from '../firebase-config';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+const Login: React.FC = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>
+    const handleSignIn = async () => {
+        try {
+          await signInWithGoogle();
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      return (
+        <div>
+          {user ? (
+            <div>
+              <p>Welcome, {user.user.displayName}</p>
+              <button onClick={() => auth.signOut()}>Sign Out</button>
+            </div>
+          ) : (
+            <button onClick={handleSignIn}>Sign In with Google</button>
+          )}
+        </div>
+      );};  
+    export default Login;
