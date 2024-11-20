ec5663d967dee9a5a1d35319ef1b9fdaf0bea749
grelmu <gmurray0505@gmail.com>
2024-11-20 10:37:16 -0500

    Delete Nefarious.test.js
    Deleting Dr.Greg's initial CI test

--- src/Nefarious.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders sign-in button', () => {
-  render(<App />);
-  const buttonElement = screen.getByText(/sign in with google/i);
-  expect(buttonElement).not.toBeInTheDocument();
-});
-
