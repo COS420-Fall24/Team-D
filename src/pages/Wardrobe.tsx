92601dd2554bebc6abf109fee9f6e617edaaa94f
Caitlin Moeykens <146303178+cam2283@users.noreply.github.com>
2024-12-04 19:19:52 -0500

    debugging and adding text to wardrobe page
    debugging some issues with importing components and modules.
    beginning to add text to page

--- src/pages/Wardrobe.tsx
+++ src/pages/Wardrobe.tsx
@@ -23,7 +23,7 @@ const WardrobePage = () => {
     const fetchClothingItems = async () => {
       try {
         const items = await fetchClothingItems(); 
-        setClothingItems(items);
+        //setClothingItems(items);
       } catch (error) {
         console.error('Error fetching items:', error);
       } finally {
@@ -54,5 +54,42 @@ const WardrobePage = () => {
     </div>
   );
 };
+
+const Header = () => {
+  return (
+    <header>
+      <h1>StyleNest Your Wardobe</h1>
+      <p>Create and organize your outfits!</p>
+      <nav>
+        <ul>
+          <li>All Items</li>
+          <li>Outfits</li>
+          <li>Categories</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+const MenuBar = () => {
+  return (
+    <nav className="menu-bar">
+      <ul>
+        <li>
+          <button>Add Item</button>
+        </li>
+        <li>
+          <button>Create Outfit</button>
+        </li>
+        <li>
+          <button>Filter</button>
+        </li>
+        <li>
+          <button>Sort</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 
 export default WardrobePage; 
